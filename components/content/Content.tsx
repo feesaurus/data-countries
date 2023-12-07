5ead081660275b49d56864bb8663e64248cbcc84
Febriana Kusumah <febriana.kusumah@nexsoft.co.id>
2023-12-07 11:03:37 +0700

    fix modal overflow

--- components/content/Content.tsx
+++ components/content/Content.tsx
@@ -17,7 +17,7 @@ const Context: React.FC<IContext> = ({ name, isOpen, isClose, data }) => {
   return (
     <>
       {isOpen &&
-        <div className="w-full absolute h-3/4 bottom-0 bg-[#454545] rounded-t-xl animate-[rotate_0.2s_ease-in] shadow-[0px_5px_15px_rgba(0, 0, 0, 0.35)]">
+        <div className="overflow-x-hidden w-full absolute h-3/4 bottom-0 bg-[#454545] rounded-t-xl animate-[rotate_0.2s_ease-in] shadow-[0px_5px_15px_rgba(0, 0, 0, 0.35)]">
           <div className="translate-x-1/2 p-px cursor-pointer" onClick={closeInfo}>
             <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 8">
               <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1" />
